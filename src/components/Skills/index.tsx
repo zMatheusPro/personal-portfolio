1cbd46488465cfd15b324b2735e7812d12710ba4
Matheus Ferreira <teteu.andrade06@gmail.com>
2022-05-19 15:07:35 -0300

    Skills update

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -2,101 +2,47 @@ import { Skill } from "./Skill";
 
 export function Skills() {
   return (
-    <div className="text-center" data-content id="skills">
-      <h1 className="text-2xl text-first-color">In planning</h1>
+    <div
+      className="grid grid-cols-1 md:grid-cols-2 justify-center gap-x-12"
+      data-content
+      id="skills"
+    >
+      <div className="skills__area">
+        <h3 className="text-lg text-center mb-6">Frontend Developer</h3>
 
-      {/* <div className="skills__area">
-        <h3 className="skills__title">Frontend Developer</h3>
-
-        <div className="skills__box">
-          <div className="skills__group">
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">HTML</h3>
-                <span className="skills__level">Basic</span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">CSS</h3>
-                <span className="skills__level">Advanced</span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">JavaScript</h3>
-                <span className="skills__level">Intermediate</span>
-              </div>
-            </div>
+        <div className="flex justify-center gap-x-12">
+          <div className="grid items-start gap-y-4">
+            <Skill level={3} skill="HTML" />
+            <Skill level={2} skill="CSS" />
+            <Skill level={2} skill="Js / Ts" />
           </div>
 
-          <div className="skills__group">
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">React</h3>
-                <span className="skills__level">Intermediate</span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">Bootstrap</h3>
-                <span className="skills__level">Intermediate</span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">Git</h3>
-                <span className="skills__level">Intermediate</span>
-              </div>
-            </div>
+          <div className="grid items-start gap-y-4">
+            <Skill level={2} skill="React Js" />
+            <Skill level={3} skill="TailwindCSS" />
+            <Skill level={1} skill="NodeJs" />
           </div>
         </div>
       </div>
 
       <div className="skills__area">
-        <h3 className="skills__title">Backend Developer</h3>
+        <h3 className="text-lg text-center mb-6">Frontend Developer</h3>
 
-        <div className="skills__box">
-          <Skill level={1} skill="NodeJs" />
-          
-
-          <div className="skills__group">
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">Python</h3>
-                <span className="skills__level">Basic</span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <i className="ri-checkbox-circle-line" />
-
-              <div>
-                <h3 className="skills__name">Node Js</h3>
-                <span className="skills__level">Intermediate</span>
-              </div>
-            </div>
+        <div className="flex justify-center gap-x-12">
+          <div className="grid items-start gap-y-4">
+            <Skill level={1} skill="PHP" />
+            <Skill level={2} skill="MySQL" />
+            <Skill level={1} skill="MongoDB" />
           </div>
+
+          <div className="grid items-start gap-y-4">
+            <Skill level={2} skill="Node Js" />
+            <Skill level={1} skill="Python" />
+            <Skill level={1} skill="NodeJs" />
+          </div>
+          
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }

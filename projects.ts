c5f5efdd02bd651d3530fd36512efe814794d3eb
Matheus Ferreira <39748103+zMatheusPro@users.noreply.github.com>
2023-07-31 06:33:02 -0300

    Update projects.ts

--- projects.ts
+++ projects.ts
@@ -2,11 +2,14 @@ import squashcodes from "./src/public/projects/squashcodes.png";
 import madland from "./src/public/projects/madland.png";
 import thelion from "./src/public/projects/thelion.png";
 import butterfly from "./src/public/projects/butterfly.png";
+import bgo from "./src/public/projects/bgo.png";
+import bvl from "./src/public/projects/bvl.png";
+import sunize from "./src/public/projects/sunize.png";
 
 const projects = [
   {
     id: "project-1",
-    title: "Addons WebStore",
+    title: "SquashCodes",
     subtitle: "Web",
     image: squashcodes,
     href: "https://squashcodes.com/",
@@ -21,16 +24,37 @@ const projects = [
   {
     id: "project-3",
     title: "Minecraft server website",
-    subtitle: "Web ( Development )",
+    subtitle: "Web ( Deprecated )",
     image: madland,
     href: "#",
   },
   {
     id: "project-4",
-    title: "Minecraft Building Studios",
+    title: "ButterFlyGames",
     subtitle: "Web",
     image: butterfly,
     href: "https://www.butterflygames.net",
+  },
+  {
+    id: "project-5",
+    title: "BGO - Brasil Gaming Online",
+    subtitle: "Web",
+    image: bgo,
+    href: "https://bgogta.com.br/",
+  },
+  {
+    id: "project-6",
+    title: "BVL - Brasil Vida Leve",
+    subtitle: "Web",
+    image: bvl,
+    href: "https://bvlmta.com.br/",
+  },
+  {
+    id: "project-7",
+    title: "Sunize Digital ( stable job )",
+    subtitle: "Web",
+    image: sunize,
+    href: "https://sunize.com.br/",
   },
 ];
 
